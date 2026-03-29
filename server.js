7bd90fb74f17e29535070f45209c0bebaa104b94
priyanb <priyanb250@gmail.com>
2026-03-29 18:12:06 +0530

    small rfactor in  server

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const connectDB = require("./config/db");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 
-
 dotenv.config();
 connectDB();
 
@@ -13,9 +12,9 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-
 app.use("/api/issues", issueRoutes);
 app.use("/api/auth", authRoutes);
+
 app.get("/", (req, res) => {
   res.send("DevTrack API running ");
 });
